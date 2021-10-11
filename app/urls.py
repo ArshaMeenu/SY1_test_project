c014ff0bb0da9cbbc0fd4c31523f81f064e1009a
ArshaMeenu <arsha@sayonetech.com>
2021-10-11 23:53:23 +0530

    userlogin api

--- app/urls.py
+++ app/urls.py
@@ -4,6 +4,8 @@ urlpatterns = [
   
     path('home',views.Home.as_view(),name = "home"),
     path('login',views.Login.as_view(),name = "login"),
+    path('userprofile',views.userProfile.as_view(),name = "userprofile"),
+
 
 
 ]
--- app/views.py
+++ app/views.py
@@ -3,17 +3,41 @@ import rest_framework
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth import authenticate
+from django.contrib.auth import login
+from django.http import HttpResponseRedirect,HttpResponse
+from django.conf import settings
+from django.contrib import messages
 
 
 class Home(APIView):
-  def get(self,request):
-    print('abc')
+  def get(self,request):    
     return render(request, "home.html")
 
 class Login(APIView):
   permission_classes = (IsAuthenticated,)
-  def get(self,request):
-    print(request.user)
+  def get(self,request):    
     return render(request,"login.html")
 
+  def post(self, request):
+        username = request.POST['username']
+        print(username)
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                messages.success(request, f' wecome {username} !!')
+                return HttpResponseRedirect('userprofile')
+            else:
+              print("inactive")
+              return HttpResponse("Inactive user.")
+        else:
+          print("create a user")
+          return HttpResponse('Create a user.')        
 
+
+
+class userProfile(APIView):
+  def get(self,request):
+    return Response("userprofile")
